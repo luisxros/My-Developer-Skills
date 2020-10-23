159c6f5edd95d2815f9faa86b63e9f1fb801a4b5
Luis Rosario <luisalmonter@gmail.com>
2020-10-22 23:13:45 -0400

    adds javascript and DOM elements

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// IPO - Input Process Out
+
+// Constants and variables (State)
+let skills;
+
+// Cached ELement References
+const $button = $('button');
+const $ul = $('ul');
+const $input = $('input');
+// Event Listeners
+$button.on('click', handleAddSkill);
+$ul.on('click', handleDelete);
+
+// functions
+
+init();
+
+function init() { 
+    skills = [];
+    render();
+}
+
+function handleAddSkill() {
+    // Store valut from the input tag insde a local variable
+    const skill = $input.val();
+    // Check to make sure we have skill data
+    if(skill) {
+        // Create UI for skill
+        const $skill = $(`<li><span>X</span>JavaScript (ES6)</li>`)
+        // Push the skill UI into the skills list
+        skills.push($skill);
+        // Clear the input tag's value
+        $input.val('');
+        // call render()
+        render()
+    }   else return;
+
+}
+
+function handleDelete() {
+    alert('Elements inside of ul tag clicked');
+}
+
+function render() {
+// Take the list of skills in the  skills array and add them to the UL tag
+if(!skills.length) $ul.css('margin-bottom', '30px')
+else $ul.css('margin-bottom', '0px');
+$ul.html(skills);
+}
